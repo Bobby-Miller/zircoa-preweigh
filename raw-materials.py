b4534dae2799a18863715d908b8f4eca4332f7f2
Bobby Miller <miller.bobby@gmail.com>
2015-09-13 15:32:02 -0500

    adding raw-material.py

---
+++ raw-materials.py
@@ -0,0 +1,15 @@
+class RawMaterial:
+    def __init__(self,
+                 psd = {0: 0.0, 10: 0.0,
+                        24: 0.0, 48: 0.0,
+                        64: 0.0, 100: 0.0,
+                        325: 0.0},
+                 mag = 0, sfh = 0, u_th = 0):
+        _psd = self.psd
+        _mag = self.mag
+        _sfh = self.sfh
+        _u_th = self.u_th
+        
+class Batch:
+    def __init__(self, 
+        
